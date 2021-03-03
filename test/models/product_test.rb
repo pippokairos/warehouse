c2f0baf50445dc58b012fb2c42236509fa83323f
Filippo Ragazzo <filipporagazzo89@gmail.com>
2021-03-03 09:34:48 +0100

    Add test cases for product validity

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,13 +1,27 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test "product shoud be valid if price is 19.99" do
+  test "product should be valid if price is 19.99" do
     product = Product.new(reference: "A001", name: "A001", category: "Electronics", price: 19.99)
     assert product.valid?
   end
 
-  test "product shoud not be valid if price is not numeric" do
+  test "product should not be valid if price is not numeric" do
     product = Product.new(reference: "A001", name: "A001", category: "Electronics", price: "Not a number")
+    assert_not product.valid?
+  end
+
+  test "product should not be valid if a field is missing" do
+    product = Product.new(name: "A001", category: "Electronics", price: 19.99)
+    assert_not product.valid?
+
+    product = Product.new(reference: "A001", category: "Electronics", price: 19.99)
+    assert_not product.valid?
+
+    product = Product.new(reference: "A001", name: "A001", price: 19.99)
+    assert_not product.valid?
+
+    product = Product.new(reference: "A001", name: "A001", category: "Electronics")
     assert_not product.valid?
   end
 end
